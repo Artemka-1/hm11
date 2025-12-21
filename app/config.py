965c381e29efef0987f9ac7571bfd9f070b97784
Artemka-1 <Artem.pridorogin0501@gmail.com>
2025-12-22 00:49:34 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,11 +1,22 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
-    REFRESH_TOKEN_EXPIRE_DAYS: int
+    database_url: str
+    secret_key: str
+    algorithm: str
+
+    mail_username: str
+    mail_password: str
+    mail_from: str
+    mail_server: str
+    mail_port: int
+
+    cloudinary_cloud_name: str
+    cloudinary_api_key: str
+    cloudinary_api_secret: str
+
+    redis_host: str
+    redis_port: int
 
     class Config:
         env_file = ".env"
