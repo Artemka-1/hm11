26e5d491a368eb59e25816baaa8df8e5faeeeadc
Artemka-1 <Artem.pridorogin0501@gmail.com>
2025-12-28 23:18:44 +0200

    Update database.py

--- app/database.py
+++ app/database.py
@@ -11,3 +11,8 @@ engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 Base = declarative_base()
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
